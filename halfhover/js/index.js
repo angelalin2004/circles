679820ca264e64ab13b367cff29dfbf130068544
angelalin2004 <adl040@ucsd.edu>
2015-08-31 23:58:29 -0700

    got layer hovering to work

--- halfhover/js/index.js
+++ halfhover/js/index.js
@@ -1,8 +1,34 @@
+$(document).ready(function(){
+  $("#test").mousemove(function(event){      
+    if (event.pageX < $("#test").width()/2+$("#test").offset().left) {
+      $("#leftc").css("z-index","100");
+    }
+  });
+});
+
+function leftEnter() {
+  console.log("left entered");
+  $("#clip-left").css("clip","rect(0px,200px,200px,0px)");
+}
+
+function rightEnter() {
+  console.log("right entered");
+  $("#clip-right").css("clip","rect(0px,200px,200px,0px)");
+}
+
+function leftLeave() {
+  $("#clip-left").css("clip","rect(0px,200px,200px,105px)");
+}
+
+function rightLeave() {
+  $("#clip-right").css("clip","rect(0px,200px,200px,105px)");
+}
+
 function halfEnter(x) {
   console.log("here"); console.log($("#left").css("clip"));
   $(x).css("clip","rect(0px,200px,200px,0px)");
 }
 function halfLeave() {
-  $("#left").css("clip","rect(0px,95px,200px,0px)");
-  $("#right").css("clip","rect(0px,200px,200px,105px)");
+  $("#clip-left").css("clip","rect(0px,95px,200px,0px)");
+  $("#clip-right").css("clip","rect(0px,200px,200px,105px)");
 }
