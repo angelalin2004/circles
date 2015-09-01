92214b491751924a3047db57725ee82d330963cd
angelalin2004 <adl040@ucsd.edu>
2015-09-01 02:42:42 -0700

    container1 done, small bug with leaving left side

--- halfhover/js/index.js
+++ halfhover/js/index.js
@@ -1,12 +1,12 @@
 var currentSide = "none"; // none, left, or right;
 
 $(document).ready(function(){
-  $("#test").mousemove(function(event){      
-    if (event.pageX < $("#test").width()/2 + $("#test").offset().left && currentSide != "right") {
+  $("#container1").mousemove(function(event){      
+    if (event.pageX < $("#container1").width()/2 + $("#container1").offset().left && currentSide != "right") {
       $("#leftc").css("z-index","10");
       $("#rightc").css("z-index","1");
     }
-    else if (event.pageX > $("#test").width()/2 + $("#test").offset().left && currentSide != "left") {
+    else if (event.pageX > $("#container1").width()/2 + $("#container1").offset().left && currentSide != "left") {
       $("#leftc").css("z-index","1");
       $("#rightc").css("z-index","10");
     }
@@ -17,28 +17,18 @@ function leftEnter() {
   currentSide = "left";
   $("#clip-left").css("clip","rect(0px,200px,200px,0px)");
 }
-
 function rightEnter() {
   currentSide = "right";
   $("#clip-right").css("clip","rect(0px,200px,200px,0px)");
 }
-
-function leftLeave() {
-  $("#clip-left").css("clip","rect(0px,200px,200px,105px)");
-}
-
-function rightLeave() {
-  $("#clip-right").css("clip","rect(0px,200px,200px,105px)");
-}
-
-function halfEnter(x) {
-  console.log("here"); console.log($("#left").css("clip"));
-  $(x).css("clip","rect(0px,200px,200px,0px)");
-}
 function halfLeave() {
-  currentSide = "none";
   $("#clip-left").css("clip","rect(0px,95px,200px,0px)");
   $("#clip-right").css("clip","rect(0px,200px,200px,105px)");
+  if (currentSide == "left") {
+    $("#rightc").css("transition", "z-index 0s");
+    $("#rightc").css("transition-delay", "1s");
+  }
   $("#leftc").css("z-index","1");
   $("#rightc").css("z-index","10");
+  currentSide = "none";
 }
