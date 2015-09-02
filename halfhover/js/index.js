43037786a9dd0dddcf2979f374371ad9bc3f4a6a
angelalin2004 <adl040@ucsd.edu>
2015-09-02 02:35:46 -0700

    finished container1. Title mouseleave is funky, abandoning

--- halfhover/js/index.js
+++ halfhover/js/index.js
@@ -1,14 +1,35 @@
 var currentSide = "none"; // none, left, or right;
 
 $(document).ready(function(){
-  $("#container1").mousemove(function(event){      
+  $("#container1").mouseenter(function(event){
+    $("#title1").css("display","none");
+    $("#title1").css("opacity","0.0");
+  });
+  /*
+  $("#container1").mouseleave(function(event){
+    console.log("left");
+    setTimeout(function(){
+      $("#title1").css("display","inline");
+    },1500);
+    setTimeout(function(){
+      $("#title1").css("opacity","0.75");
+    },3000); 
+  });
+  */ 
+  $("#container1").mousemove(function(event){ 
+    $("#title1").css("display","none");
+    $("#title1").css("opacity","0.0");
     if (event.pageX < $("#container1").width()/2 + $("#container1").offset().left && currentSide != "right") {
       $("#leftc").css("z-index","10");
       $("#rightc").css("z-index","1");
+      $("#left-text").css("z-index","30");
+      $("#right-text").css("z-index","20");
     }
     else if (event.pageX > $("#container1").width()/2 + $("#container1").offset().left && currentSide != "left") {
       $("#leftc").css("z-index","1");
       $("#rightc").css("z-index","10");
+      $("#left-text").css("z-index","20");
+      $("#right-text").css("z-index","30");
     }
   });
 });
@@ -16,17 +37,17 @@ $(document).ready(function(){
 function leftEnter() {
   currentSide = "left";
   $("#clip-left").css("clip","rect(0px,200px,200px,0px)");
-  $("#textc").css("width","200px");
-  $("#textc").css("overflow","visible");
+  $("#left-text").css("opacity","1.0");
 }
 function rightEnter() {
   currentSide = "right";
   $("#clip-right").css("clip","rect(0px,200px,200px,0px)");
+  $("#right-text").css("opacity","1.0");
 }
 function halfLeave() {
   $("#clip-left").css("clip","rect(0px,95px,200px,0px)");
-  $("#textc").css("width","0px");
-  $("#textc").css("overflow","hidden");
+  $("#left-text").css("opacity","0.0");
   $("#clip-right").css("clip","rect(0px,200px,200px,105px)");
+  $("#right-text").css("opacity","0.0");
   currentSide = "none";
 }
